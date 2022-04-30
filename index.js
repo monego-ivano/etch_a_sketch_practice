85db2a979905d86281ec20c69a71ff0afb9d53e7
Ivano <ivanomonego@yahoo.it>
2022-04-30 13:04:05 +0200

    Added event listener to cells and resetGrid func

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 'use strict'
 
 const grid = document.querySelector('.grid');
+let resetBtn = document.querySelector('.reset');
 
+
+// CREATE GRID SPECIFYING THE NUMBER OF CELLS
 
 function createGrid(cells) {
+    let gridCell;
     for (let i = 0; i < (cells * cells); i++) {
-        const gridCell = document.createElement('div');
+        gridCell = document.createElement('div');
         gridCell.setAttribute('class', 'grid-cell');
         grid.appendChild(gridCell);
-    }
+    };
 
     
 }
 
+// RESET THE GRID CREATING A NEW BLANK ONE
 
+function resetGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    createGrid(16);
+}
 
 
 createGrid(16);
+
+
+grid.addEventListener('mouseover', (e) => {
+    if(e.target.matches('.grid-cell')) {
+        e.target.setAttribute('class', 'permahover');
+        e.stopPropagation();
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    resetGrid();
+});
