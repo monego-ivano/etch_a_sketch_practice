c14af45318525262d4e27b3a7e59533d3ca110b3
Ivano <ivanomonego@yahoo.it>
2022-05-05 23:35:28 +0200

    added slider and computed side length

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 'use strict'
 
+
 const grid = document.querySelector('.grid');
 let resetBtn = document.querySelector('.reset');
+let slider = document.querySelector('.slider');
+let gridSide = slider.value;
+let gridPx = document.querySelector('.grid-px');
+gridPx.textContent = `${slider.value}px`;
 
 
 // CREATE GRID SPECIFYING THE NUMBER OF CELLS
 
 function createGrid(cells) {
     let gridCell;
+    let side = 500 / cells;
     for (let i = 0; i < (cells * cells); i++) {
         gridCell = document.createElement('div');
+        gridCell.style.height = `${side}px`;
+        gridCell.style.width = `${side}px`;
         gridCell.setAttribute('class', 'grid-cell');
         grid.appendChild(gridCell);
     };
@@ -23,18 +31,18 @@ function resetGrid() {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-    createGrid(16);
+    createGrid(gridSide);
 }
 
 
-createGrid(16);
+createGrid(gridSide);
 
 
 // ADDED MOUSEOVER TO DRAW
 
 grid.addEventListener('mouseover', (e) => {
     if(e.target.matches('.grid-cell')) {
-        e.target.setAttribute('class', 'permahover');
+        e.target.style.backgroundColor = '#333';
         e.stopPropagation();
     }
 });
@@ -45,3 +53,9 @@ grid.addEventListener('mouseover', (e) => {
 resetBtn.addEventListener('click', () => {
     resetGrid();
 });
+
+slider.addEventListener('change', () => {
+    gridSide = slider.value;
+    gridPx.textContent = `${slider.value}px`;
+    resetGrid();
+})
